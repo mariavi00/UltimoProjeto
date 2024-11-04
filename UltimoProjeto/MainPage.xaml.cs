b8b27eef823b02a641a5ca48c9e64e7dc926ee84
UltimoProjeto <mavi.gouveia678@gmail.com>
2024-11-04 11:35:18 -0300

    04/11

--- UltimoProjeto/MainPage.xaml.cs
+++ UltimoProjeto/MainPage.xaml.cs
@@ -2,9 +2,86 @@
 
 public partial class MainPage : ContentPage
 {
+	bool estaMorto=false;
+	bool estaPulando=false;
+	const int tempoEntreFrames=25;
+		int velocidade1=0;
+		int velocidade2=0;
+		int velocidade3=0;
+		int velocidade=0;
+		int larguraJanela=0;
+		int alturaJanela=0;
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		CorrigeTamanhoCenario(w, h);
+		CalculaVelocidade(w);
+	}
+	void CalculaVelocidade(double w)
+	{
+		velocidade1=(int)(w*0.001);
+		velocidade2=(int)(w*0.004);
+		velocidade3=(int)(w*0.008);
+		velocidade=(int)(w*0.01);
+	}
+	void CorrigeTamanhoCenario(double w, double h)
+	{
+		foreach (var lua in HSLayer1.Children)
+			(lua as Image). WidthRequest = w;
+		foreach (var estrelas in HSLayer2.Children)
+			(estrelas as Image). WidthRequest = w;
+		foreach (var arvores in HSLayer3.Children)
+			(arvores as Image). WidthRequest = w;
+		foreach (var chao in HSLayerChao.Children)
+			(chao as Image). WidthRequest = w;
+
+		HSLayer1.WidthRequest = w;
+		HSLayer2.WidthRequest = w;
+		HSLayer3.WidthRequest = w;
+		HSLayerChao.WidthRequest = w;
+	}
+	void GerenciaCenarios()
+	{
+		MoveCenario();
+		GerenciaCenario(HSLayer1);
+		GerenciaCenario(HSLayer2);
+		GerenciaCenario(HSLayer3);
+		GerenciaCenario(HSLayerChao);
+	}
+	void MoveCenario()
+	{
+		HSLayer1.TranslationX-=velocidade1;
+		HSLayer2.TranslationX-=velocidade2;
+		HSLayer3.TranslationX-=velocidade3;
+		HSLayerChao.TranslationX-=velocidade;
+	}
+	void GerenciaCenario(HorizontalStackLayout hsl)
+	{
+		var view = (hsl.Children.First() as Image);
+		if (view.WidthRequest + hsl.TranslationX<0)
+		{
+			hsl.Children.Remove(view);
+			hsl.Children.Add(view);
+			hsl.TranslationX=view.TranslationX;
+		}
+	}
+	async Task Desenha()
+	{
+		while(!estaMorto)
+		{
+			GerenciaCenarios();
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		Desenha();
+    }
+
 }
 
--- UltimoProjeto/obj/Debug/net8.0-ios/iossimulator-x64/UltimoProjeto.AssemblyInfo.cs
+++ UltimoProjeto/obj/Debug/net8.0-ios/iossimulator-x64/UltimoProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UltimoProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8b072db505688091351dfb3d98f42dcde945f0de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3136a511fda49fa59cedcd9cc1f6287bc9416b95")]
 [assembly: System.Reflection.AssemblyProductAttribute("UltimoProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UltimoProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
